6c607bc598491108bbe2b2c791b9b05ee686004e
Duong Hoang <2024.duonghoang@gmail.com>
2024-07-01 14:21:46 +0700

    -Merge latest main. -Rename artifact id. -Rename application class name.

--- src/main/java/ktc/nhom1ktc/MoneyTrackerApplication.java
+++ src/main/java/ktc/nhom1ktc/MoneyTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Nhom1KtcApplication {
+public class MoneyTrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Nhom1KtcApplication.class, args);
+        SpringApplication.run(MoneyTrackerApplication.class, args);
     }
 
 }
