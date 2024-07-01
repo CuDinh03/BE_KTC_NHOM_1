d6284042015c2f6e26fa3f23fc604326b0d10ce5
CuDinh03 <dinhhuycu0305@gmail.com>
2024-07-01 19:22:51 +0700

    update user CRUD

--- src/main/java/ktc/nhom1ktc/service/IUserService.java
+++ src/main/java/ktc/nhom1ktc/service/IUserService.java
@@ -1,4 +1,18 @@
 package ktc.nhom1ktc.service;
 
+import ktc.nhom1ktc.entity.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface IUserService {
+    Users getByID(UUID id);
+    Users create(Users user);
+    Users update(UUID id, Users user);
+    void delete(UUID id);
+    List<Users> getAll();
+    Page<Users> getAllPageable(Pageable pageable);
+    void open(UUID id);
 }
